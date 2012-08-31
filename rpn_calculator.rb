3258a1595bcd6411f36cf21fa73c3e2e521dcfee
arn-e <fostvedt.arne@gmail.com>
2012-08-30 22:13:12 -0400

    removed debug code

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -19,12 +19,3 @@ class RPNCalculator
 	end
 
 end
-
-calc = RPNCalculator.new
-calc.evaluate('1 2 3 + -')
-calc.evaluate('2 5 *')
-calc.evaluate('50 20 -')
-calc.evaluate('70 10 4 + 5 * -')
-calc.evaluate('0')
-calc.evaluate('1 1 +')
-calc.evaluate('20 10 5 4 + * -')
