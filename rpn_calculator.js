6ae76d451cb9e37196796043697267765ee4ccfa
Arne Fostvedt <fostvedt.arne@gmail.com>
2012-09-20 16:09:05 -0400

    updated javascript indentation

--- rpn_calculator.js
+++ rpn_calculator.js
@@ -7,21 +7,21 @@ Array.prototype.remove = function(from, to) {
 
 //PostFix Calculator
 function evaluate(input){
-	var elem = input.split(' ');
-	return calculate(elem);
+    var elem = input.split(' ');
+    return calculate(elem);
 }
 
 function calculate(elem){
-	if (elem.length > 1){
-		for (var i=0; i< (elem.length); i++){
-			if (elem[i] === "+" || elem[i] === "-" || elem[i] === "*"){
-				elem[i-2] = eval(elem[i-2].toString() + elem[i].toString()+elem[i-1].toString());
-				elem.remove(i-1) && elem.remove(i-1); 
-				calculate(elem);
-			}
-		}
-	}
-	return Number(elem[0]);
+    if (elem.length > 1){
+        for (var i=0; i< (elem.length); i++){
+            if (elem[i] === "+" || elem[i] === "-" || elem[i] === "*"){
+                elem[i-2] = eval(elem[i-2].toString() + elem[i].toString()+elem[i-1].toString());
+                elem.remove(i-1) && elem.remove(i-1); 
+                calculate(elem);
+            }
+        }
+    }
+    return Number(elem[0]);
 }
 
 console.log(evaluate("5 2 3 4 + * -"));
